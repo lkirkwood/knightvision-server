d7b009e5e382e0473549c6d61702b9c4aff67582
Linus Kirkwood <linuskirkwood@gmail.com>
2025-05-07 23:22:51 +1000

    Update server to use new chess detection api

--- src/server.py
+++ src/server.py
@@ -1,8 +1,16 @@
 from flask import Flask, Request, Response
 from PIL import Image
 from io import BytesIO
+from ultralytics import YOLO
+from chess_detector import detect_chess_board
+import os
 
 app = Flask(__name__)
+
+if not ("MODEL_PATH" in app.config and os.path.isfile(app.config["MODEL_FILE"])):
+    raise RuntimeError("Missing MODEL_PATH configuration value.")
+
+app.config["MODEL"] = YOLO(app.config["MODEL_PATH"])
 
 
 @app.post("/parse-board")
@@ -12,6 +20,11 @@ def parse_board(req: Request) -> Response:
             f"Unexpected MIME type {req.mimetype}; Expected PNG or JPG", 400
         )
 
-    image = Image.open(BytesIO(req.data))
-    ...  # TODO call detect_chess_board with Image
-    return Response("Not implemented", 501)
+    if "orientation" not in req.args:
+        return Response("Expected a board orientation parameter.", 400)
+
+    result = detect_chess_board(
+        app.config["MODEL"], Image.open(BytesIO(req.data)), req.args["orientation"]
+    )
+
+    return Response(result.fen, 200)
